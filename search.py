ed079bd72e41c1654443c91fed8ed5fad28627a1
soulintzis <esoulintzhs@gmail.com>
2020-10-23 20:42:36 +0300

    Import data to elasticsearch

---
+++ search.py
@@ -0,0 +1,32 @@
+import sys
+import json
+import pandas as pd
+from pprint import pprint
+from elasticsearch import Elasticsearch
+
+filename = 'movies.csv'
+es = Elasticsearch(
+    ['localhost'],
+    port=9200
+
+)
+
+def load_and_convert_CSV_to_JSON(filename):
+    dataFrame = pd.read_csv("movies.csv") 
+
+    result = dataFrame.to_json(orient="records")
+    parsed = json.loads(result)
+    json_str = json.dumps(parsed, indent=4)  
+
+    return json.loads(json_str)
+
+def insert_movies_to_elasticsearch(object):
+    counter = 0
+    for movie in object:
+        es.index(index='movies', doc_type='movie', id=counter, body=movie)
+        counter += 1
+    print('Movies inserted successfully')
+
+def main():
+    movies = load_and_convert_CSV_to_JSON(filename)
+    insert_movies_to_elasticsearch(movies)
