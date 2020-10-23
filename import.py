ccf50ae710398c197b47f5b6421e8e39660af4c8
soulintzis <esoulintzhs@gmail.com>
2020-10-24 01:50:43 +0300

    rename file

--- import.py
+++ import.py
@@ -1,7 +1,6 @@
 import sys
 import json
 import pandas as pd
-from pprint import pprint
 from elasticsearch import Elasticsearch
 
 filename = 'movies.csv'
@@ -16,7 +15,7 @@ def load_and_convert_CSV_to_JSON(filename):
 
     result = dataFrame.to_json(orient="records")
     parsed = json.loads(result)
-    json_str = json.dumps(parsed, indent=4)  
+    json_str = json.dumps(parsed, indent=4)
 
     return json.loads(json_str)
 
