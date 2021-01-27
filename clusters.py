7685d7692bfc78080754a2b16330711c75b94f30
Dimitris Ioannou <43514376+dimitrisioan@users.noreply.github.com>
2021-01-27 21:26:25 +0200

    Add 3rd query clusters and update search

--- clusters.py
+++ clusters.py
@@ -4,11 +4,17 @@ import pprint as pp
 import pandas as pd
 from tabulate import tabulate
 from elasticsearch import Elasticsearch
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
 import ast
+import math
 import numpy as np
+import pickle
 
-ratings_df = pd.read_csv("ratings.csv") 
-movies_df = pd.read_csv("movies.csv") 
+ratings_df = pd.read_csv("ratings.csv")
+clusters = pd.read_csv("clusters.csv")
+movies_df = pd.read_csv("movies.csv")
 movies_df['genres'] = movies_df['genres'].map(lambda x: x.split('|'))
 
 print(ratings_df)
@@ -16,23 +22,73 @@ print(movies_df)
 
 user_ids = ratings_df['userId'].unique()
 
-genres = set()
+all_genres = set()
 for i, row in movies_df.iterrows():
-    genres.update(row['genres'])
+    all_genres.update(row['genres'])
 
-clusters = pd.DataFrame(columns=['user_ids', 'genre', 'avg'])
+clusters_dict = []
+for user in user_ids:
+	for genre in all_genres:
+	    clusters_dict.append(
+	        {
+	            'userId': user,
+	            'genre': genre,
+	            'avg': 0.0
+	        }
+	    )
 
-for user_id in user_ids:
-    for genre in genres:
-        row = [user_id, genre, 0.0]
-        clusters.loc[len(clusters)] = row
-        
-print(clusters)
+# clusters = pd.DataFrame(clusters_dict)
 
-# user_id, avg_rating, genre
+# for user in user_ids:
+# 	ratings = []
+# 	# get users ratings
+# 	u_ratings = ratings_df.loc[ratings_df['userId'] == user]
+# 	for index, rating in u_ratings.iterrows():
+# 		movie = rating['movieId'].astype(int)
+# 		genres = movies_df.loc[movies_df['movieId'] == movie]['genres'].reset_index()
+# 		genres = genres['genres'][0]
+# 		for genre in genres:
+# 			ratings.append(
+# 				{
+# 					'genre': genre,
+# 					'rating': rating['rating']
+# 				}
+# 			)
 
-# gia kathe rating
-#     if user & genre in df
-#         update
-#     else 
-#         insert user, genre
+# 	for genre in all_genres:
+# 		ratings_ = list()
+# 		sum = 0
+# 		count = 0
+# 		for rating in ratings:
+# 			if rating.get('genre') == genre:
+# 				sum += rating.get('rating')
+# 				count += 1
+# 		if count != 0:
+# 			avg = sum / count
+# 			clusters.loc[(clusters['userId'] == user) & (clusters['genre'] == genre), 'avg'] = avg
+# 			print(clusters.loc[(clusters['userId'] == user) & (clusters['genre'] == genre)])
+# clusters.to_csv(r'clusters.csv')
+
+del clusters['index']
+del clusters['genre']
+
+sparce_matrix = clusters.values
+
+sparce_matrix = np.split(sparce_matrix[:,1], np.unique(sparce_matrix[:, 0], return_index=True)[1][1:])
+
+num_of_clusters = 2*len(all_genres)
+kmeans = KMeans(n_clusters=num_of_clusters).fit(sparce_matrix)
+clusters_of_users = kmeans.predict(sparce_matrix)
+
+
+clusters_of_users_map = {k: [] for k in range(0, num_of_clusters)}
+for c in range(0, num_of_clusters):
+	users_on_the_cluster = []
+	for user_id, cluster in enumerate(clusters_of_users):
+		if cluster == c:
+			users_on_the_cluster.append(user_id)
+			clusters_of_users_map[c].append(user_id)
+
+print(clusters_of_users_map)
+pickle.dump(clusters_of_users_map, open("cluster.pkl", "wb"))
+pickle.dump(clusters_of_users, open("user_cluster.pkl", "wb"))
--- search_version1.py
+++ search_version1.py
@@ -6,6 +6,7 @@ from tabulate import tabulate
 from elasticsearch import Elasticsearch
 import ast
 import numpy as np
+import pickle
 
 filename = 'movies.csv'
 
@@ -19,6 +20,9 @@ DOC_TYPE = 'movie'
 
 user_weight = 1
 avg_weight = 0.5
+
+def average(lst):
+    return sum(lst) / len(lst)
 
 def convert_DataFrame_to_JSON(df):
     result = df.to_json(orient="records")
@@ -38,9 +42,9 @@ def searchMovies(query):
     _source = elastic_df[['_source']].copy()
 
     # convert movies data to dict
-    js=json.loads(_source.to_json()) 
+    js=json.loads(_source.to_json())
 
-    # the the first value from the dict 
+    # the the first value from the dict
     # contains all the movies data
     key, value = js.popitem()
 
@@ -67,16 +71,16 @@ def searchMovies(query):
 
 
 
-ratings_df = pd.read_csv("ratings.csv") 
+ratings_df = pd.read_csv("ratings.csv")
 movies_avg_ratings = ratings_df.groupby(['movieId'])['rating'].mean()
 
-user_ratings = ratings_df.loc[ratings_df['userId'] == 86]
-user_ratings.rename(columns={'rating': 'user_rating'}, inplace=True)
+#user_ratings = ratings_df.loc[ratings_df['userId'] == 86]
+#user_ratings.rename(columns={'rating': 'user_rating'}, inplace=True)
 
 #------------------------------------------------------------------------------------------
 
 def load_and_convert_CSV_to_JSON(filename):
-    dataFrame2 = pd.read_csv("ratings.csv") 
+    dataFrame2 = pd.read_csv("ratings.csv")
 
     result = dataFrame2.to_json(orient="records")
     parsed = json.loads(result)
@@ -84,7 +88,7 @@ def load_and_convert_CSV_to_JSON(filename):
 
     return json.loads(json_str)
 
-ratings = pd.read_csv("ratings.csv") 
+ratings = pd.read_csv("ratings.csv")
 
 
 
@@ -92,7 +96,8 @@ if __name__ == '__main__':
     while(True):
         print('1--Search for a movie with the default metric(BM25).')
         print('2--Search for a movie with your custom metric.')
-        print('3--Exit.')
+        print('3--Search for a movie with the new metric after clustering.')
+        print('4--Exit.')
         while(True):
                 try:
                     decision = int(input('Select from menu: '))
@@ -142,13 +147,66 @@ if __name__ == '__main__':
                 max_score = results.loc[results['_score'].idxmax()]._score
 
                 results = pd.merge(results, movies_avg_ratings, on='movieId')
-                results = pd.merge(results, user_ratings[['user_rating', 'movieId']], on='movieId', how='left').fillna(0)
+                results = pd.merge(results, rating[['rating', 'movieId']], on='movieId', how='left').fillna(0)
                 results.rename(columns={'rating': 'avg_rating'}, inplace=True)
                 results['score'] = (results['avg_rating'] + user_weight * results['user_rating'] + avg_weight * results['_score'])/max_score
                 results.sort_values(by = 'score', inplace=True, ascending=False)
                 print(results)
-            
+        
         elif(decision == 3):
+            cluster = pickle.load(open("cluster.pkl", "rb"))
+            user_cluster = pickle.load(open("user_cluster.pkl", "rb"))
+            requested_id = int(input('Please give your user_id:'))
+            user_belongs = user_cluster[requested_id]
+            other_users = cluster[user_belongs]
+            print('User belongs in cluster: ' + str(user_belongs))
+            print('The other users on that cluster are:' + str(other_users))
+
+            print('What are you looking for?')
+            search_term = input()
+            query = {
+                # "from": 0,
+                # "size": 20,
+                "query":
+                {
+                    "simple_query_string":
+                    {
+                        "query": search_term,
+                        "fields": ["title", "genres"]
+                    }
+                }
+            }
+            results = searchMovies(query)
+            movies_to_rate = results['movieId'].tolist()
+            user_ratings = {rating: 0.0 for rating in movies_to_rate}
+            for movie in movies_to_rate:
+                movie_rts = []
+                if not ((ratings_df['userId'] == requested_id) & (ratings_df['movieId'] == movie)).any():
+                    for o_user in other_users:
+                        if ((ratings_df['userId'] == o_user) & (ratings_df['movieId'] == movie)).any():
+                            movie_rating = float(ratings_df[(ratings_df['userId'] == o_user) & (ratings_df['movieId'] == movie)]['rating'])
+                            movie_rts.append(movie_rating)
+                    if len(movie_rts) != 0:
+                        user_ratings[movie] = average(movie_rts)
+                    else:
+                        user_ratings[movie] = 0
+                else:
+                    user_ratings[movie] = float(ratings_df[(ratings_df['userId'] == requested_id) & (ratings_df['movieId'] == movie)]['rating'])
+
+            print(user_ratings)
+            user_ratings_df = pd.DataFrame(user_ratings.items(), columns = ['movieId', 'user_rating'])
+
+            max_score = results.loc[results['_score'].idxmax()]._score
+
+            results = pd.merge(results, movies_avg_ratings, on='movieId')
+            results.rename(columns={'rating': 'avg_rating'}, inplace=True)
+            results = pd.merge(results, user_ratings_df[['user_rating', 'movieId']], on='movieId', how='left')
+            results['score'] = (results['avg_rating'] + user_weight * results['user_rating'] + avg_weight * results['_score'])/max_score
+            results.sort_values(by = 'score', inplace=True, ascending=False)
+
+            print(results)
+            
+        elif(decision == 4):
             exit(0)
         
         else:
